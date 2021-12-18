a7abbb0ee1fb65bc38fa23329af1041ff0145781
sh4869 <sh4869@ajou.ac.kr>
2021-12-18 15:25:24 +0900

    React hooks

--- star_rating/src/App.js
+++ star_rating/src/App.js
@@ -1,10 +1,14 @@
 import React from "react";
 import ColorList from "./components/ColorList.js";
 import AddColorForm from "./components/AddColorForm";
+import CheckBox from "./components/Checkbox";
+import Numbers from "./components/Numbers.js";
 
 function App() {
   return (
     <>
+      <Numbers />
+      <CheckBox />
       <AddColorForm />
       <ColorList />
       </>
---
+++ star_rating/src/components/Numbers.js
@@ -0,0 +1,11 @@
+import React, { useReducer } from 'react';
+
+function Numbers() {
+    const [number, setNumber] = useReducer(
+        (number, newNumber) => number+newNumber,
+        0
+    )
+    return <h1 onClick={() => setNumber(30)}>{number}</h1>
+}
+
+export default Numbers;
